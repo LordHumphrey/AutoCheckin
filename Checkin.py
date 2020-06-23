c120dcaa0b385570ea39fe18279b41ae0e2fc9a8
Tao <2591036017@qq.com>
2020-06-23 22:19:35 +0800

    [新增功能](1.0.0):

---
+++ Checkin.py
@@ -0,0 +1,47 @@
+import requests
+import os
+import yagmail
+
+username = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
+login_url = os.environ["LOGIN"]
+check_in = os.environ["CHECKIN"]
+email = os.environ["EMAIL"]
+email_password = os.environ["EMAILPASSWORD"]
+
+
+def main():
+    s = requests.Session()
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': login_url,
+        'Connection': 'close',
+        'Referer': login_url,
+        'Content-Length': '0',
+    }
+    params = {
+        "email": username,
+        "passwd": password
+    }
+
+    s.post(login_url, headers=header, params=params)
+    result = s.post(check_in, headers=header)
+    send_email(result.text.encode('ascii').decode('unicode_escape'))
+
+
+def send_email(contents):
+    yag = yagmail.SMTP(user=email, password=email_password, host='smtp.office365.com', port=587,
+                       smtp_starttls=True, smtp_ssl=False)
+
+    yag.send('2591036017@qq.com', '每日签到', username + login_url)
+
+
+if __name__ == "__main__":
+    main()
+
+# https://touhou.plus/user/checkin
+# https://touhou.plus/auth/login
