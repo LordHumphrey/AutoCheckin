608157f83cab2b11da6182118a8f5b7569655bdd
Tao <2591036017@qq.com>
2020-09-20 12:47:48 +0800

    [新增功能](3.0.0): 支持多机场签到

--- Checkin.py
+++ Checkin.py
@@ -1,42 +1,74 @@
+from json import JSONDecodeError
 import requests
 import os
 import yagmail
 import json
-
-check_in = os.environ["CHECKIN"]
-cookie = os.environ["COOKIE"]
-email = os.environ["EMAIL"]
-email_password = os.environ["EMAILPASSWORD"]
-target_email = os.environ["TARGETEMAIL"]
+import re
 
 
-def main():
-    s = requests.Session()
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-        'Accept-Encoding': 'gzip, deflate',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Connection': 'close',
-        'Content-Length': '0',
-        'cookie': cookie
-    }
+class Email:
+    def __init__(self, email, email_password, target_email):
+        self.email = email
+        self.email_password = email_password
+        self.target_email = target_email
+        self.email_content = ""
 
-    result = s.post(check_in, headers=header)
-    result_dict = json.loads(result.content)
-    email_message = ""
-    for key, value in result_dict.items():
-        email_message = email_message + str(key) + ":" + str(value) + " ; "
-    send_email(email_message)
+    def send_email(self):
+        print("send {} to {} ".format(self.email_content, self.target_email))
+        yag = yagmail.SMTP(user=email, password=email_password, host='smtp.office365.com', port=587,
+                           smtp_starttls=True, smtp_ssl=False)
+        yag.send(self.target_email, '每日签到', self.email_content)
 
 
-def send_email(contents):
-    yag = yagmail.SMTP(user=email, password=email_password, host='smtp.office365.com', port=587,
-                       smtp_starttls=True, smtp_ssl=False)
+class CheckIn:
+    def __init__(self, check_in_url, cookie, email):
+        self.email = email
+        self.session = requests.Session()
+        self.check_in_url = check_in_url
+        self.cookie = cookie
+        self.result_dict = ""
+        self.header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Connection': 'close',
+            'Content-Length': '0',
+            'cookie': cookie
+        }
 
-    yag.send(target_email, '每日签到', contents)
+    def get_name(self):
+        get_name_url = str(self.check_in_url).replace("/user/checkin", "/user")
+        html_content = self.session.get(get_name_url, headers=self.header).content.decode("utf-8")
+        return re.findall(r'<title>(.*?)</title>', html_content)[0].replace("&mdash;",
+                                                                            "—").replace(
+            "用户中心 — ", "") + ": "
+
+    def main(self):
+        self.email.email_content += self.get_name()
+        result = self.session.post(self.check_in_url, headers=self.header)
+        try:
+            self.result_dict = json.loads(result.content)
+        except JSONDecodeError:
+            self.email.email_content += "Cookie失效或者登陆失败"
+        else:
+            for key, value in self.result_dict.items():
+                self.email.email_content = self.email.email_content + str(key) + ":" + str(value) + " ; "
+            self.email.email_content += "\n"
 
 
 if __name__ == "__main__":
-    main()
+    check_in_list = os.environ["CHECKIN"].split(",")
+    cookie_list = os.environ["COOKIE"].split(",")
+    email = os.environ["EMAIL"]
+    email_password = os.environ["EMAILPASSWORD"]
+    target_email = os.environ["TARGETEMAIL"]
+    email_instance = Email(email, email_password, target_email)
+
+    for check_in, cookie in zip(check_in_list, cookie_list):
+        my_heck_in = CheckIn(check_in, cookie, email_instance)
+        my_heck_in.main()
+        del my_heck_in
+
+    email_instance.send_email()
