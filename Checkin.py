e392a92e748a62e7131c7e1039542081e35bc6b3
Tao <2591036017@qq.com>
2020-09-21 20:24:55 +0800

    [新增功能](3.0.1): 多机场登录版本

--- Checkin.py
+++ Checkin.py
@@ -20,12 +20,18 @@ class Email:
 
 
 class CheckIn:
-    def __init__(self, check_in_url, cookie, email):
-        self.email = email
-        self.session = requests.Session()
-        self.check_in_url = check_in_url
-        self.cookie = cookie
+    def __init__(self, login_url, account, password, email):
+        self.login_url = login_url
+        self.password = password
+        self.account = account
         self.result_dict = ""
+        self.session = requests.Session()
+        self.email = email
+        self.checkin_url = str(login_url).replace("/auth/login", "/user/checkin")
+        self.login_data = {
+            "email": account,
+            "passwd": password
+        }
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
             'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -34,19 +40,22 @@ class CheckIn:
             'X-Requested-With': 'XMLHttpRequest',
             'Connection': 'close',
             'Content-Length': '0',
-            'cookie': cookie
         }
 
+    def login(self):
+        self.session.post(self.login_url, headers=self.header, data=self.login_data)
+
     def get_name(self):
-        get_name_url = str(self.check_in_url).replace("/user/checkin", "/user")
+        get_name_url = str(self.checkin_url).replace("/user/checkin", "/user")
         html_content = self.session.get(get_name_url, headers=self.header).content.decode("utf-8")
         return re.findall(r'<title>(.*?)</title>', html_content)[0].replace("&mdash;",
                                                                             "—").replace(
             "用户中心 — ", "") + ": "
 
     def main(self):
+        self.login()
         self.email.email_content += self.get_name()
-        result = self.session.post(self.check_in_url, headers=self.header)
+        result = self.session.post(self.checkin_url, headers=self.header)
         try:
             self.result_dict = json.loads(result.content)
         except JSONDecodeError:
@@ -54,20 +63,28 @@ class CheckIn:
         else:
             for key, value in self.result_dict.items():
                 self.email.email_content = self.email.email_content + str(key) + ":" + str(value) + " ; "
-            self.email.email_content += "\n"
+        self.email.email_content += "\n"
 
+
+class WebsiteAccount:
+    def __init__(self, url, account, password):
+        self.url = url
+        self.account = account
+        self.password = password
 
 
 if __name__ == "__main__":
-    check_in_list = os.environ["CHECKIN"].split(",")
-    cookie_list = os.environ["COOKIE"].split(",")
+    login_url_list = os.environ["LOGIN_URL"].split(",")
+    account_list = os.environ["ACCOUNT"].split(",")
+    password_list = os.environ["PASSWORD"].split(",")
     email = os.environ["EMAIL"]
     email_password = os.environ["EMAILPASSWORD"]
     target_email = os.environ["TARGETEMAIL"]
+
     email_instance = Email(email, email_password, target_email)
 
-    for check_in, cookie in zip(check_in_list, cookie_list):
-        my_heck_in = CheckIn(check_in, cookie, email_instance)
+    for login_url, account, password in zip(login_url_list, account_list, password_list):
+        my_heck_in = CheckIn(login_url, account, password, email_instance)
         my_heck_in.main()
         del my_heck_in
 
